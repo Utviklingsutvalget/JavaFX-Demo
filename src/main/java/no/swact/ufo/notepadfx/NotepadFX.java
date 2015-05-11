eaa5957058de72b8fb207b9aad28c527cf24e1b8
eivindveg <eivindveg@hotmail.com>
2015-05-11 19:34:20 +0200

    add boilerplate for time consuming or researchy things

--- src/main/java/no/swact/ufo/notepadfx/NotepadFX.java
+++ src/main/java/no/swact/ufo/notepadfx/NotepadFX.java
@@ -1,17 +1,22 @@
 package no.swact.ufo.notepadfx;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.RadioMenuItem;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import org.apache.commons.io.IOUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 
 public class NotepadFX extends Application {
 
@@ -19,7 +24,6 @@ public class NotepadFX extends Application {
     private TextArea textArea;
     @FXML
     private ToggleGroup fontSizeGroup;
-
 
     public static void main(String[] args) {
         Application.launch(NotepadFX.class);
@@ -34,7 +38,42 @@ public class NotepadFX extends Application {
         NotepadFX controller = loader.getController();
         primaryStage.setScene(new Scene(root));
         controller.setup();
+
+        createOnCloseListener(primaryStage);
+
         primaryStage.show();
+    }
+
+    private void createOnCloseListener(final Stage primaryStage) {
+        Platform.setImplicitExit(false);
+
+        primaryStage.setOnCloseRequest(event -> {
+            Alert choiceDialog = new Alert(Alert.AlertType.CONFIRMATION, "Would you like to save before exiting?", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+            Optional<ButtonType> optionalResponse = choiceDialog.showAndWait();
+
+            ButtonType response = optionalResponse.get();
+
+            if (response == ButtonType.CANCEL) {
+                event.consume();
+            }
+        });
+    }
+
+    private void saveFile(File target, String content) {
+        try (FileOutputStream output = new FileOutputStream(target)) {
+            IOUtils.write(content, output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String loadFile(File target) {
+        try (FileInputStream input = new FileInputStream(target)) {
+            return IOUtils.toString(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private void setup() {
