3b5d8bc6d65fe15fc4047279731e8ee8bc2b3ea6
eivindveg <eivindveg@hotmail.com>
2015-05-12 15:44:46 +0200

    include the things done during the demo presentation

--- src/main/java/no/swact/ufo/notepadfx/NotepadFX.java
+++ src/main/java/no/swact/ufo/notepadfx/NotepadFX.java
@@ -2,13 +2,19 @@ package no.swact.ufo.notepadfx;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import org.apache.commons.io.IOUtils;
 
 import java.io.File;
@@ -21,9 +27,19 @@ import java.util.Optional;
 public class NotepadFX extends Application {
 
     @FXML
+    private Menu progress;
+    @FXML
+    private MenuItem saveFile;
+    @FXML
+    private MenuItem openFile;
+    @FXML
     private TextArea textArea;
     @FXML
     private ToggleGroup fontSizeGroup;
+
+    private ScheduledService<Void> service;
+
+    private ObjectProperty<File> selectedFile = new SimpleObjectProperty<>();
 
     public static void main(String[] args) {
         Application.launch(NotepadFX.class);
@@ -81,6 +97,66 @@ public class NotepadFX extends Application {
     private void setup() {
         textArea.setFont(getFontFromMenuItem((RadioMenuItem) fontSizeGroup.getSelectedToggle()));
         createFontListener();
+        setupLoadFile();
+        setupSaveFile();
+        selectedFile.addListener((observable, oldValue, newValue) -> {
+            File file = observable.getValue();
+            if (newValue == null) {
+                return;
+            }
+            if (service != null) {
+                service.cancel();
+            }
+            service = new ScheduledService<Void>() {
+                @Override
+                protected Task<Void> createTask() {
+                    return new Task<Void>() {
+                        @Override
+                        protected Void call() throws Exception {
+                            System.out.println("Saving");
+                            saveFile(file, textArea.getText());
+                            System.out.println("It seems we have saved a file");
+
+                            Thread.sleep(1000L);
+                            succeeded();
+                            return null;
+                        }
+                    };
+                }
+            };
+            service.setPeriod(Duration.millis(5000));
+            service.setOnRunning(event1 -> progress.setVisible(true));
+            service.setOnSucceeded(event -> {
+                System.out.println("'sup?");
+                progress.setVisible(false);
+            });
+            service.start();
+        });
+    }
+
+
+    private void setupSaveFile() {
+        saveFile.setOnAction(event -> {
+            FileChooser chooser = new FileChooser();
+            chooser.setTitle("Select file to save");
+            File file = chooser.showSaveDialog(null);
+            if (file == null) {
+                return;
+            }
+            selectedFile.setValue(file);
+        });
+    }
+
+    private void setupLoadFile() {
+        openFile.setOnAction(event -> {
+            FileChooser chooser = new FileChooser();
+            chooser.setTitle("Select file to load");
+            File file = chooser.showOpenDialog(null);
+            if (file == null) {
+                return;
+            }
+            textArea.setText(loadFile(file));
+        });
     }
 
     private void createFontListener() {
